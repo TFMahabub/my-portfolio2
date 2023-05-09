52ccc231824e2a29f40b32169de95355f737d924
Mahabub Alam <rjmahabub543@gmail.com>
2023-05-10 00:00:37 +0600

    modify about section

--- src/Components/Pages/Home/About/About.js
+++ src/Components/Pages/Home/About/About.js
@@ -14,7 +14,7 @@ const About = () => {
             Mahabub is a Front End web developer who creates good looking modern and high-performance web-applications for any kind of business.
             </p>
             <p className='text-lg text-secondary '>
-            <br /> Mahabub specializes in ReactJS and Redux but is not limited to them.
+            <br /> Mahabub specializes in ReactJS, Next.js and Redux but is not limited to them.
             </p>
             <p className='text-lg text-secondary '>
             <br /> Mahabub understands how important is UX design for a web application, and how to create a web application from sketch and implement it. He also knows how to create a pixel-perfect website exactly by design, also web application created by mahabub are fully responsive(for computers, tablets, mobile devices) and user-oriented.
